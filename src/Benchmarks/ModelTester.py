6577aaf173935bfea489c9a54328798bf741f156
Beniamin Pasak <94671940+BPasak@users.noreply.github.com>
2026-01-19 01:14:50 +0100

    Updated benchmarking to allow for more resilient and detailed model analysis

--- src/Benchmarks/ModelTester.py
+++ src/Benchmarks/ModelTester.py
@@ -1,15 +1,16 @@
 import os
-import sys
 import random
+import sys
 from datetime import datetime
 from pathlib import Path
 
 import numpy as np
-from torch_geometric.data import Batch as PyGBatch, Data as PyGGraph
+from torch_geometric.data import Batch as PyGBatch
 from tqdm.auto import tqdm
 
-from AIPowerMeter.deep_learning_power_measure.power_measure import parsers, experiment
+from AIPowerMeter.deep_learning_power_measure.power_measure import experiment, parsers
 from Benchmarks.OnModule import countParams, measure_graph_construction_latency, measure_model_runtime
+from Datasets.base import Dataset, DatasetMode
 from Models.base import BaseModel
 from utils.systemSpecs import get_system_specs
 
@@ -34,9 +35,12 @@ class ModelTester:
                 f'Experiment Results performed on {current_time}\n\n'
                 )
 
-            f.write(f'Model Specifications\n')
-            f.write(f'Name: {self.model.__class__.__name__}\n')
-            f.write(f'Parameter Count: {countParams(self.model)}\n')
+            f.write(f'Model Name: {self.model.__class__.__name__}\n')
+            try:
+                f.write(f'Number of Parameters: {countParams(self.model):,}\n')
+            except:
+                f.write(f'Number of Parameters: Parameters not initialized.\n')
+
             f.write(f'\n')
             f.write('-'*25)
             f.write('\n\n')
@@ -48,13 +52,57 @@ class ModelTester:
                 f.write(f' GPU {gpu["id"]}: {gpu["name"]}\n')
             f.write('\n')
 
+    def detail_model_parameters(self):
+        with open(self._performance_results_path(), 'a') as f:
+            f.write('-'*25)
+            f.write('\n\n')
+            f.write(f'Parameter Count Analysis\n')
+            f.write(f'Total Parameters: {countParams(self.model):,}\n')
+            for idx, tensor in enumerate(self.model.parameters()):
+                f.write(f'   Component {idx}: {tensor.numel()}\n')
+            f.write('\n')
+
+    def _write_down_value(self, name, value, indent: int):
+        if isinstance(value, dict):
+            with open(self._performance_results_path(), 'a') as f:
+                f.write(' '*indent + f"{name}:\n")
+
+            for key, val in value.items():
+                self._write_down_value(key, val, indent + 3)
+        else:
+            with open(self._performance_results_path(), 'a') as f:
+                f.write(' '*indent + f"{name}: {value}\n")
+
+    def record_model_hyperparameters(self, hyperparameters: dict):
+
+        assert isinstance(hyperparameters, dict), "Hyperparameters must be a dictionary."
+
+        with open(self._performance_results_path(), 'a') as f:
+            f.write('-'*25)
+            f.write('\n\n')
+            f.write(f'Model Hyperparameters\n')
+
+        for key, value in hyperparameters.items():
+            self._write_down_value(key, value, indent = 3)
+
+        with open(self._performance_results_path(), 'a') as f:
+            f.write('\n')
+
     def _performance_results_path(self):
         return self.results_dir_path / "performance_results.txt"
 
     def _get_power_consumption_path(self):
         return self.results_dir_path / "power_consumption"
 
-    def test_model_performance(self, data: list[PyGGraph] | PyGBatch, batch_sizes: list[int] = None, test_sizes: list[int] = None):
+    def test_model_performance(
+        self,
+        dataset: Dataset,
+        mode: DatasetMode,
+        sampled_count = 10,
+        batch_sizes: list[int] = None,
+        test_sizes: list[int] = None,
+        device = "cpu"
+    ):
 
         if batch_sizes is None:
             batch_sizes = [1]
@@ -62,24 +110,29 @@ class ModelTester:
         if test_sizes is None:
             test_sizes = [100] * len(batch_sizes)
 
-        if isinstance(data, PyGBatch):
-            data = data.to_data_list()
-
-        batches: dict[int, list[PyGBatch]] = {}
-        for batch_size, test_size in zip(batch_sizes, test_sizes):
-            batches[batch_size] = []
-            for i in range(test_size):
-                sampled_graphs = random.sample(data, batch_size)
-                batch = PyGBatch.from_data_list(sampled_graphs)
-                batches[batch_size].append(batch)
+        assert len(batch_sizes) == len(test_sizes), "The number of batch sizes must be equal to the number of test sizes."
 
         with open(self._performance_results_path(), 'a') as f:
             f.write('-'*25)
             f.write(f'\nModel Performance Results\n')
-            f.write(f'\nGraph Construction Latency (ms) - investigated on {len(data)} graphs\n')
+            f.write(f'\nGraph Construction Latency (ms) - investigated on {dataset.get_mode_length(mode)} graphs\n')
+
+        graph_transform = dataset.transform
+        dataset.transform = None
+        sampled_graphs = random.sample(range(dataset.get_mode_length(mode)), sampled_count)
+        unprocessed_graphs = [dataset.get_mode_data(mode, i) for i in sampled_graphs]
+        dataset.transform = graph_transform
 
         print("Analyzing Graph Construction Latency...")
-        times = measure_graph_construction_latency(self.model, data)
+        times, processed_data = measure_graph_construction_latency(
+            data_transform = dataset.transform,
+            data = unprocessed_graphs,
+            return_processed_data = True
+        )
+
+        processed_data = [sample for sample in processed_data if sample is not None]
+        if len(processed_data) < len(unprocessed_graphs):
+            print(f"Warning - {len(unprocessed_graphs) - len(processed_data)} graphs were not processed successfully - None samples returned.")
 
         with open(self._performance_results_path(), 'a') as f:
             f.write(f'    AVG | STD | MAX | MIN\n')
@@ -87,12 +140,20 @@ class ModelTester:
 
             f.write('\nInference Latency (ms)\n')
 
+        batches: dict[int, list[PyGBatch]] = {}
+        for batch_size, test_size in zip(batch_sizes, test_sizes):
+            batches[batch_size] = []
+            for i in range(test_size):
+                sampled_graphs = random.sample(processed_data, batch_size)
+                batch = PyGBatch.from_data_list(sampled_graphs)
+                batches[batch_size].append(batch)
+
         self.model.eval()
         print("Analyzing Inference Latency for different batch sizes...")
         for batch_size, batch_list in tqdm(batches.items(), total = len(batches)):
             with open(self._performance_results_path(), 'a') as f:
                 f.write(f'\n Batch Size: {batch_size} - investigated on {len(batch_list)} batches\n')
-                times = measure_model_runtime(self.model, batch_list)
+                times = measure_model_runtime(self.model, batch_list, device = device)
 
                 f.write(f'   AVG | STD | MAX | MIN\n')
                 f.write(f'   {sum(times) / len(times):.2f} | {np.std(times):.2f} | {max(times):.2f} | {min(times):.2f}\n')
--- src/Benchmarks/OnModule.py
+++ src/Benchmarks/OnModule.py
@@ -1,4 +1,5 @@
 import time
+from typing import Callable
 
 import torch
 from torch_geometric.data import Data as PyGGraph
@@ -8,24 +9,32 @@ from torch import nn
 from Models.base import BaseModel
 
 @torch.no_grad()
-def measure_graph_construction_latency(model: BaseModel, data: list[PyGGraph]):
+def measure_graph_construction_latency(
+    data: list[PyGGraph],
+    data_transform: Callable[[PyGGraph], PyGGraph],
+    return_processed_data = False
+):
     times = []
+    processed_data = []
     for graph in data:
-        graph = graph.to(model.device)
         start_time = time.perf_counter()
-        model.data_transform(graph)
+        processed_graph = data_transform(graph)
         end_time = time.perf_counter()
-        graph.cpu()
 
+        if return_processed_data:
+            processed_data.append(processed_graph)
         times.append(1000*(end_time - start_time))
+
+    if return_processed_data:
+        return times, processed_data
 
     return times
 
 @torch.no_grad()
-def measure_model_runtime(model: BaseModel, data: list[PyGBatch]):
+def measure_model_runtime(model: BaseModel, data: list[PyGBatch], device = "cpu"):
     times = []
     for batch in data:
-        batch = batch.to(model.device)
+        batch = batch.to(device)
         start_time = time.perf_counter()
         model(batch)
         end_time = time.perf_counter()
